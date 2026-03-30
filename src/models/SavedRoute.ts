ab2484e3c352e96f963cb12e0b5358f1ecfddf79
Sophia <sf313612@gmail.com>
2026-03-31 00:32:44 +0300

    update saved routes

--- src/models/SavedRoute.ts
+++ src/models/SavedRoute.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const savedRouteSchema = new mongoose.Schema({
     userId: { type: String, required: true },
     route: [{type: String, required: true }],
-    totalDistance: {type: Number, required: true } //shoud it be String?
+    totalDistance: {type: Number, required: true }
 });
 
 export default mongoose.model("SavedRoute", savedRouteSchema);
